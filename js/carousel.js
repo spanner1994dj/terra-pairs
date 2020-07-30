3bd9fad913fa2548d6455de29ae1a81cf51c6317
Stewart Hitchcock <Stewart.Hitchcock@gmail.com>
2020-07-30 13:55:05 +0100

    Content additions

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        // autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        appenddots:$()
+    });
+  });
+
+// $('.carousel').slick({
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     arrows: false,
+//     dots: true,
+//     appenddots:$()
+// });
+
