b539253c37c637e61c0374a833e2792b3c9edd11
spanner1994dj <66678588+spanner1994dj@users.noreply.github.com>
2020-08-19 17:03:50 +0100

    php adjustment

--- contact.php
+++ contact.php
@@ -2,45 +2,45 @@
 $pageTitle = "Contact Us";
 
 include("inc/header.php"); ?>
-<div class="media">
-   <h1>Contact Us</h1>
-         <a class="temp" type="button" href="index.php"> Temp home button</a>
-         <p>If you have any questions please get in contact with us using the form below and we will respond as soon as possible.</p>
-         <div class="contact-form">
-            <div class="container">
-               <form action="action_page.php">
+
+<h1>Contact Us</h1>
+      <a class="temp" type="button" href="index.php"> Temp home button</a>
+      <p>If you have any questions please get in contact with us using the form below and we will respond as soon as possible.</p>
+      <div class="contact-form">
+         <div class="container">
+            <form action="action_page.php">
+         
+               <label for="fname">First Name:</label>
+               <input type="text" id="fname" name="firstname" placeholder="Your name..">
             
-                  <label for="fname">First Name:</label>
-                  <input type="text" id="fname" name="firstname" placeholder="Your name..">
-               
-                  <label for="lname">Last Name:</label>
-                  <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+               <label for="lname">Last Name:</label>
+               <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
-                  <label for="email">Email:</label>
-                  <input type="text" id="email-add" name="mail" placeholder="Email..">
-               
-                  <label for="tel-num">Telephone Number:</label>
-                  <input type="text" id="numb" name="telephone" placeholder="Telephone Number..">
-               
-                  <label for="subject">Subject:</label>
-                  <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-                  <input type="checkbox" class="check" id="myCheck" onclick="myFunction()"> <label class=myCheck> I consent to my email and personal data stored by Terracotta Catering.</label><br>
-
-                  <!--Use this site key in the HTML code your site servers to users!!!
-                  6Lcg47kZAAAAAAQkalO5bE9FATnS9tQS7LdYNYbB -->
-                  
-                  <!-- Secret key for communication between site and recpatcha
-                  6Lcg47kZAAAAAFDzknj-OHvS8AGvcSmErQJXc9-c -->
-
-                  <input type="submit" value="Submit">
-               </form>
+               <label for="email">Email:</label>
+               <input type="text" id="email-add" name="mail" placeholder="Email..">
             
-            </div>
-         </div>
-         <div id="mapid">
+               <label for="tel-num">Telephone Number:</label>
+               <input type="text" id="numb" name="telephone" placeholder="Telephone Number..">
+            
+               <label for="subject">Subject:</label>
+               <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
 
-         </div>
-</div>
+               <input type="checkbox" class="check" id="myCheck" onclick="myFunction()"> <label class=myCheck> I consent to my email and personal data stored by Terracotta Catering.</label><br>
 
+               <!--Use this site key in the HTML code your site servers to users!!!
+               6Lcg47kZAAAAAAQkalO5bE9FATnS9tQS7LdYNYbB -->
+               
+               <!-- Secret key for communication between site and recpatcha
+               6Lcg47kZAAAAAFDzknj-OHvS8AGvcSmErQJXc9-c -->
+
+               <input type="submit" value="Submit">
+            </form>
+           
+         </div>
+      </div>
+      <div class="map">
+         <div class="mapid">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2420.7066983715727!2d1.262040815956824!3d52.64721183454589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9e173705fe203%3A0xf26790a125dad856!2s16%20Zobel%20Cl%2C%20Norwich%20NR3%202BY!5e0!3m2!1sen!2suk!4v1597841065230!5m2!1sen!2suk" width="500" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+         </div>
+      </div>
 <?php include("inc/footer.php"); ?>
