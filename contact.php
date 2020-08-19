b699264426f8cdce67bd00613393ee8b85c5a7de
Stewart Hitchcock <Stewart.Hitchcock@gmail.com>
2020-08-19 13:48:39 +0100

    commit

--- contact.php
+++ contact.php
@@ -2,44 +2,45 @@
 $pageTitle = "Contact Us";
 
 include("inc/header.php"); ?>
-
-<h1>Contact Us</h1>
-      <a class="temp" type="button" href="index.php"> Temp home button</a>
-      <p>If you have any questions please get in contact with us using the form below and we will respond as soon as possible.</p>
-      <div class="contact-form">
-         <div class="container">
-            <form action="action_page.php">
-         
-               <label for="fname">First Name:</label>
-               <input type="text" id="fname" name="firstname" placeholder="Your name..">
+<div class="media">
+   <h1>Contact Us</h1>
+         <a class="temp" type="button" href="index.php"> Temp home button</a>
+         <p>If you have any questions please get in contact with us using the form below and we will respond as soon as possible.</p>
+         <div class="contact-form">
+            <div class="container">
+               <form action="action_page.php">
             
-               <label for="lname">Last Name:</label>
-               <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-               <label for="email">Email:</label>
-               <input type="text" id="email-add" name="mail" placeholder="Email..">
-            
-               <label for="tel-num">Telephone Number:</label>
-               <input type="text" id="numb" name="telephone" placeholder="Telephone Number..">
-            
-               <label for="subject">Subject:</label>
-               <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-               <input type="checkbox" class="check" id="myCheck" onclick="myFunction()"> <label class=myCheck> I consent to my email and personal data stored by Terracotta Catering.</label><br>
-
-               <!--Use this site key in the HTML code your site servers to users!!!
-               6Lcg47kZAAAAAAQkalO5bE9FATnS9tQS7LdYNYbB -->
+                  <label for="fname">First Name:</label>
+                  <input type="text" id="fname" name="firstname" placeholder="Your name..">
                
-               <!-- Secret key for communication between site and recpatcha
-               6Lcg47kZAAAAAFDzknj-OHvS8AGvcSmErQJXc9-c -->
+                  <label for="lname">Last Name:</label>
+                  <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
-               <input type="submit" value="Submit">
-            </form>
-           
+                  <label for="email">Email:</label>
+                  <input type="text" id="email-add" name="mail" placeholder="Email..">
+               
+                  <label for="tel-num">Telephone Number:</label>
+                  <input type="text" id="numb" name="telephone" placeholder="Telephone Number..">
+               
+                  <label for="subject">Subject:</label>
+                  <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+                  <input type="checkbox" class="check" id="myCheck" onclick="myFunction()"> <label class=myCheck> I consent to my email and personal data stored by Terracotta Catering.</label><br>
+
+                  <!--Use this site key in the HTML code your site servers to users!!!
+                  6Lcg47kZAAAAAAQkalO5bE9FATnS9tQS7LdYNYbB -->
+                  
+                  <!-- Secret key for communication between site and recpatcha
+                  6Lcg47kZAAAAAFDzknj-OHvS8AGvcSmErQJXc9-c -->
+
+                  <input type="submit" value="Submit">
+               </form>
+            
+            </div>
          </div>
-      </div>
-      <div id="mapid">
+         <div id="mapid">
 
-      </div>
+         </div>
+</div>
 
 <?php include("inc/footer.php"); ?>
--- services.php
+++ services.php
@@ -3,8 +3,29 @@ $pageTitle = "Our Services";
 
 include("inc/header.php"); ?>
 
-<div class="test">
+<div class="main-srvc">
+<div class="seperator"></div>
+    <div class="media content-srvc">
+        
+        <h1> Our Services </h1>
 
+        <div class="cards">
+            <div class="card">
+                <div class="srvc-img-1"></div>
+                <p>Lorem Ipsum</p>
+            </div>    
+            <div class="card">
+                <div class="srvc-img-2"></div>
+                <p>Lorem Ipsum</p>
+            </div>
+
+            <div class="card">
+                <div class="srvc-img-3"></div>
+                <p>Lorem Ipsum</p>
+            </div>
+
+        </div>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
